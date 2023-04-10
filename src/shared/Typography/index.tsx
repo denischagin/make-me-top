3d2c8d9ce396a13d1932e8ecbcb2ff79b4ab3d6e
Матвей Кусков <73180631+matveikuskov@users.noreply.github.com>
2023-04-10 14:39:03 +0300

    MMT-11: исправил повторение имени блока в стилях, добавил возможность указать цвет для typography

--- src/shared/Typography/index.tsx
+++ src/shared/Typography/index.tsx
@@ -2,31 +2,43 @@ import { TypographyInterface } from "./interfaces";
 import "./styles.scss";
 
 export const Typography = (props: TypographyInterface) => {
-  const getTypographyClass = (variant: string) => {
-    if (variant === "h2") {
+  const getTypographyVariant = () => {
+    if (props.variant === "h2") {
       return "typography typography--h2";
     }
 
-    if (variant === "medium16") {
+    if (props.variant === "medium16") {
       return "typography typography--medium16";
     }
 
-    if (variant === "medium14") {
+    if (props.variant === "medium14") {
       return "typography typography--medium14";
     }
 
-    if (variant === "regular16") {
+    if (props.variant === "regular16") {
       return "typography typography--regular16";
     }
 
-    if (variant === "regular14") {
+    if (props.variant === "regular14") {
       return "typography typography--regular14";
     }
 
     return "typography typography--h1";
   };
 
+  const getTypographyClass = () => {
+    if (props.color === "orange") {
+      return `${getTypographyVariant()} typography--orange`;
+    }
+
+    if (props.color === "black") {
+      return `${getTypographyVariant()} typography--black`;
+    }
+
+    return getTypographyVariant();
+  };
+
   return (
-    <div className={getTypographyClass(props.variant)}>{props.children}</div>
+    <div className={getTypographyClass()}>{props.children}</div>
   );
 };
