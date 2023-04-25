c916e9433607ca28da0122b53472171aa5e56bc7
Матвей Кусков <73180631+matveikuskov@users.noreply.github.com>
2023-04-25 14:48:06 +0300

    MMT-20: убрал StarBig в Rating для сокращения и читаемости кода

--- src/shared/Rating/index.tsx
+++ src/shared/Rating/index.tsx
@@ -1,5 +1,4 @@
 import { ReactComponent as StarIcon } from "@shared/images/star.svg";
-import { ReactComponent as StarBigIcon } from "@shared/images/star-big.svg";
 import { bem } from "@shared/utils/bem";
 
 import { RatingInterface } from "./interfaces";
@@ -19,25 +18,27 @@ export const Rating = (props: RatingInterface) => {
     <div className={block()}>
       {
         !props.reflect &&
-        (
-          props.size === "small" || props.size === "medium"
-            ? <StarIcon className={element("star", { color: starColor })} />
-            : <StarBigIcon className={element("star", { color: starColor })} />
-        )
+        <StarIcon
+          className={element("star", {
+            color: starColor,
+            size: size
+          })}
+        />
       }
-      <p className={element("score", {
-        size: size,
-        color: scoreColor
-      })}>
+      <p
+        className={element("score", {
+          size: size,
+          color: scoreColor
+        })}
+      >
         { props.rating.toFixed(1) }
       </p>
       {
         props.reflect &&
-        (
-          props.size === "large"
-            ? <StarBigIcon className={element("star", { color: starColor })} />
-            : <StarIcon className={element("star", { color: starColor })} />
-        )
+        <StarIcon className={element("star", {
+          color: starColor,
+          size: size
+        })} />
       }
     </div>
   );
