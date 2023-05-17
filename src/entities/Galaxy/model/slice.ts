ef553929f269da23c98a99c263eadda40eb151eb
ISextra <sabinixkensey@gmail.com>
2023-05-17 12:40:47 +0300

    MMT-29 - фикс видимости прогресса для каждой кланеты, добавление статусов Not found и Loading для галактики

--- src/entities/Galaxy/model/slice.ts
+++ src/entities/Galaxy/model/slice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-import { getGalaxy } from "../api/getGalaxy";
+import { getGalaxy } from "@entities/Galaxy/api/getGalaxy";
 import {GalaxyType} from "@entities/Galaxy/model/types";
 
 const galaxySlice = createSlice({
@@ -12,6 +12,13 @@ const galaxySlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
+    builder.addCase(getGalaxy.pending, (state: GalaxyType) => {
+
+      state.galaxyId = 0;
+      state.galaxyName = "Loading...";
+      state.orbitList = [];
+    });
+
     builder.addCase(getGalaxy.fulfilled, (state: GalaxyType, action) => {
 
       state.orbitList = action.payload.orbitList;
@@ -19,10 +26,10 @@ const galaxySlice = createSlice({
       state.galaxyName = action.payload.galaxyName;
     });
 
-    builder.addCase(getGalaxy.rejected, (state: GalaxyType, action) => {
+    builder.addCase(getGalaxy.rejected, (state: GalaxyType) => {
 
       state.galaxyId = 0;
-      state.galaxyName = String(action.payload);
+      state.galaxyName = "Not found";
       state.orbitList = [];
     });
   },
