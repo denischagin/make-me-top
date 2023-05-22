4ec0412b521a12837a56d3af952a18769dc75000
isolovev <sabinixkensey@gmail.com>
2023-05-22 15:58:02 +0300

    MMT-29: добавлено отображение прогресса для каждой планеты (WIP)

--- src/shared/StarProgress/index.tsx
+++ src/shared/StarProgress/index.tsx
@@ -10,14 +10,36 @@ const StarProgress: React.FC<IStarProgressProps> = (props) => {
 
     const [block, element] = bem("star-progress");
 
+    const percentageProgressToSVGPath = (percentageProgress: number): string => {
+        let pixelProgress = 80 / 100 * (100 - percentageProgress);
+        const offsetForException100 = 20;
+
+        if (percentageProgress > 95) {
+            pixelProgress -= offsetForException100;
+        }
+
+        return String(`
+            M 20 60 
+            C 20 ${pixelProgress + 20}, 100 ${pixelProgress + 20}, 100 60
+            L 120 120
+            L 0 120
+            z
+            `);
+    }
+
     return (
         <div className={block()}>
-            <div
+            <svg
                 className={element("progress")}
-                style={{
-                    height: `${100 - percentageProgress}%`,
-                }}
-            />
+                xmlns="http://www.w3.org/2000/svg"
+                width="120px"
+                height="120px"
+            >
+                <path
+                    d={percentageProgressToSVGPath(percentageProgress)}
+                    stroke="black"
+                    fill="#7c7abd"/>
+            </svg>
         </div>
     );
 };
