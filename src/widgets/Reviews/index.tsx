381875f62a37bb229eb90d1e159ba2d99e11f03a
Матвей Кусков <73180631+matveikuskov@users.noreply.github.com>
2023-07-28 18:10:18 +0300

    https://jira.mmtr.ru/browse/MMT-40 добавлен функционал кпопки расширения кол ва элементов

--- src/widgets/Reviews/index.tsx
+++ src/widgets/Reviews/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useAppSelector } from '@app/providers/store/hooks';
@@ -18,10 +19,13 @@ import { bem } from '@shared/utils/bem';
 import { buttonSize } from '@shared/Button/interfaces';
 import { typographyVariant } from '@shared/Typography/interfaces';
 
+import { DEFAULT_EL_LIMIT } from './model';
+
 import './styles.scss';
 
 export const Reviews = () => {
     const [block, element] = bem('reviews');
+    const [limitEl, setLimitEl] = useState<number>(DEFAULT_EL_LIMIT);
 
     const location = useLocation();
 
@@ -29,7 +33,6 @@ export const Reviews = () => {
     const reviews = isExplorerRegex.test(location.pathname)
         ? useAppSelector(explorerCardInfoSelector)
         : useAppSelector(keeperCardInfoSelector);
-
 
     const {
         feedback,
@@ -48,7 +51,7 @@ export const Reviews = () => {
                     </Typography>
                     <div className={element('cards')}>
                         {
-                            feedback?.map((item) => (
+                            feedback.slice(0, limitEl)?.map((item) => (
                                 <ReviewCard
                                     key={item.courseId}
                                     review={item}
@@ -56,12 +59,26 @@ export const Reviews = () => {
                             ))
                         }
                     </div>
-                    <div className={element('button', 'mt-5')}>
-                        <Button
-                            title="Показать ещё"
-                            size={buttonSize.large}
-                        />
-                    </div>
+                    {
+                        (feedback?.length > limitEl) &&
+                        <div className={element('button', 'mt-5')}>
+                            <Button
+                                title="Показать ещё"
+                                size={buttonSize.large}
+                                onClick={() => { setLimitEl(limitEl + DEFAULT_EL_LIMIT); }}
+                            />
+                        </div>
+                    }
+                    {
+                        (feedback?.length === limitEl) || (limitEl > feedback?.length) &&
+                        <div className={element('button', 'mt-5')}>
+                            <Button
+                                title="Скрыть"
+                                size={buttonSize.large}
+                                onClick={() => setLimitEl(DEFAULT_EL_LIMIT)}
+                            />
+                        </div>
+                    }
                 </div>
             }
         </>
