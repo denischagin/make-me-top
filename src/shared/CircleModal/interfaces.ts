89dc5ddd2ddb237867456872a12671a800aaf093
Иван Соловьев <isolovev@ad.local>
2023-07-31 13:32:42 +0300

    https://jira.mmtr.ru/browse/MMT-38
    + перечесляемый тип о видах доступа перенесен в интерфейс компонента CircleModal
    
    + добавлены добавлены значения в параметры компонента CircleModal

--- src/shared/CircleModal/interfaces.ts
+++ src/shared/CircleModal/interfaces.ts
@@ -1,8 +1,30 @@
 import { ReactNode } from 'react';
 
+import { UserProgress } from '@entities/user/model/types';
+
+import { ILastChosenStar } from '@entities/galaxy/model/types';
+
+import { ModalAlertVariants } from '@shared/ModalAlert/interfaces';
+import { RequiredStarInterface } from '@shared/RequiredStarsList/interfaces';
+
 export interface ModalInterface {
     header: string;
     isLocked?: boolean;
+    data?: {
+        lastChosenStar: ILastChosenStar,
+        userProgress: UserProgress,
+    };
     children: ReactNode;
     onClose: (dispatch: any) => any;
+}
+
+export interface ModalAlert {
+    variant: ModalAlertVariants;
+    systemList?: Array<RequiredStarInterface>;
+}
+
+export enum ModalAccessStatus {
+    opened = 'Доступ открыт',
+    closed_choseKeeper = 'Для изучения данной звезды, выберите хранителя и отправьте заявку',
+    closed_needStars = 'Для изучения данной звезды, необходимы знания о звездах:',
 }
