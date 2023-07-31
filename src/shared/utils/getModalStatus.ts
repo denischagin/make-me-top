299732e8ba3f1741d815b136c7a35e134b475b06
Иван Соловьев <isolovev@ad.local>
2023-07-31 14:00:28 +0300

    https://jira.mmtr.ru/browse/MMT-38
    + создание функции getModalStatus для получения статуса модульного окна по прогрессу пользователя на определенной системе

---
+++ src/shared/utils/getModalStatus.ts
@@ -0,0 +1,34 @@
+import { UserProgress } from '@entities/user/model/types';
+
+import { ILastChosenStar } from '@entities/galaxy/model/types';
+
+import { getRequiredStars } from '@shared/utils/getRequiredStars';
+
+import { ModalAccessStatus } from '@shared/CircleModal/interfaces';
+
+interface GetModalStatus {
+    lastChosenStar: ILastChosenStar,
+    userProgress: UserProgress,
+}
+
+export function getModalStatus(params: GetModalStatus): ModalAccessStatus {
+    const {
+        lastChosenStar,
+        userProgress,
+    } = params;
+
+    const notStudiedParentDependencies = getRequiredStars({
+        lastChosenStar,
+        userProgress,
+    });
+
+    if (notStudiedParentDependencies.length) {
+        return ModalAccessStatus.closed_needStars;
+    }
+
+    if (!notStudiedParentDependencies.length && lastChosenStar.isLocked) {
+        return ModalAccessStatus.closed_choseKeeper;
+    }
+
+    return ModalAccessStatus.opened;
+}
