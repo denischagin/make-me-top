3b590f8a27a190af7f45cf3b889fc3cbfdac63e0
Иван Соловьев <isolovev@ad.local>
2023-07-31 13:23:10 +0300

    https://jira.mmtr.ru/browse/MMT-38
    + расширен интерфейс компонента ModalAlert, добавлен добавлен перечисляемый тип для вариантов сообщения

--- src/shared/ModalAlert/interfaces.ts
+++ src/shared/ModalAlert/interfaces.ts
@@ -1,6 +1,17 @@
 import { ReactNode } from 'react';
 
+import { SystemDependencyType } from '@entities/galaxy/model/types';
+
+import { RequiredStarInterface } from '@shared/RequiredStarsList/interfaces';
+
 export interface ModalAlertInterface {
-    title: string;
+    starStatus:  string;
+    title?: string;
+    dependencies?: Array<SystemDependencyType>,
     children?: ReactNode;
+}
+
+export enum ModalAlertVariants {
+    choseKeeper = 'choseKeeper',
+    needAnotherSystem = 'needAnotherSystem',
 }
